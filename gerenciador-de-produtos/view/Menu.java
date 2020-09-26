dde6e770d6f1dc50cbabe4cc311a6d5eb9ed2dc1
mauriciomendonca <mauricio.mendonca@upperds.com>
2020-09-26 16:56:49 -0300

    Menu principal

---
+++ gerenciador-de-produtos/view/Menu.java
@@ -0,0 +1,10 @@
+package view;
+
+public class Menu {
+    public void exibirMenuPrincipal() {
+        System.out.println("[1] Cadastrar produto");
+        System.out.println("[2] Listar produto");
+        System.out.println("[3] Remover produto");
+        System.out.println("[0] Sair");
+    }
+}
