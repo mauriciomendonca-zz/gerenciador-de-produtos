124668b45556a119a994024bf3273cc0cbaafc62
mauriciomendonca <mauricio.mendonca@upperds.com>
2020-09-26 16:21:07 -0300

    Alterado nome da pasta

--- gerenciador-de-produtos/models/PlacaDeVideo.java
+++
@@ -1,7 +0,0 @@
-package models;
-
-public class PlacaDeVideo extends Produto {
-    public PlacaDeVideo(String nome, String fabricante, double valor) {
-        super(nome, fabricante, valor);
-    }
-}
--- gerenciador-de-produtos/models/Processador.java
+++
@@ -1,7 +0,0 @@
-package models;
-
-public class Processador extends Produto {
-    public Processador(String nome, String fabricante, double valor) {
-        super(nome, fabricante, valor);
-    }
-}
