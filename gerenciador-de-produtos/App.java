3d8829325c6d1226e43fec9cb75140dd938f224c
mauriciomendonca <mauriciomendonca14@gmail.com>
2020-09-27 14:58:51 -0300

    Reorganizando classe App

--- gerenciador-de-produtos/App.java
+++ gerenciador-de-produtos/App.java
@@ -1,44 +1,40 @@
 import java.io.IOException;
-import java.util.Scanner;
 
-import model.Database;
+import model.DatabaseProduto;
+import model.DatabaseProcessador;
 import controller.Processador;
+import view.Cadastro;
 import view.Menu;
 
 
 class App {
     public static void main(String[] args) throws IOException {
-        Scanner scanner = new Scanner(System.in);
         Menu menu = new Menu();
-        Database database = new Database();
+        Cadastro cadastro = new Cadastro();
+        DatabaseProduto databaseProduto = new DatabaseProduto();
+        DatabaseProcessador databaseProcessador = new DatabaseProcessador();
 
-        menu.exibirHeaderMenu();
+        menu.exibirMenuHeader();
+
         while (true) {
-            menu.exibirMenuPrincipal();
+            int opcaoPrincipal = menu.exibirMenuPrincipal();
 
-            System.out.print("Selecione a opção desejada: ");
-            int opcao = scanner.nextInt();
-
-            if (opcao == 1) {
-                menu.exibirMenuOpcao01();
-                System.out.print("Selecione a opção desejada: ");
-                int opcao1 = scanner.nextInt();
-
-                if (opcao1 == 1) {
-                    System.out.print("Digite o nome do processador: ");
-                    String nome = scanner.next();
-
-                    System.out.print("Digite a fabricante do processador: ");
-                    String fabricante = scanner.next();
-
-                    // System.out.print("Digite o valor do processador: ");
-                    // double valor = scanner.nextDouble();
-
-                    // Processador processador = new Processador(nome, fabricante, valor);
-                }
+            if (opcaoPrincipal == 1) {
+                int opcaoCadastrar = menu.exibirMenuCadastrarProduto();
+                if (opcaoCadastrar == 1)
+                    cadastro.cadastrarProcessador();
+                else if (opcaoCadastrar == 2)
+                    cadastro.cadastrarPlacaDeVideo();
+                else if (opcaoCadastrar == 3)
+                    cadastro.cadastrarDiscoRigido();
+            } else if (opcaoPrincipal == 2) {
+                System.out.println("Listar produtos");
+            } else if (opcaoPrincipal == 3) {
+                System.out.println("Remover produto");
+            } else if (opcaoPrincipal == 4) {
+                System.out.println("Obrigado por utilizar o Gerenciador de Produtos!");
                 break;
             }
         }
-
     }
 }
